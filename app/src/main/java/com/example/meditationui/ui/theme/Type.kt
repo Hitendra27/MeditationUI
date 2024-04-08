84b356b308a71d117050e8d4c2e2e055d826c8fa
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-04-08 21:33:33 +0100

    fix: fixed the bug

--- app/src/main/java/com/example/meditationui/ui/theme/Type.kt
+++ app/src/main/java/com/example/meditationui/ui/theme/Type.kt
@@ -2,33 +2,41 @@ package com.example.meditationui.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.meditationui.R
+
+val gothicA1 = FontFamily(
+    listOf(
+        Font(R.font.gothic_regular, FontWeight.Normal),
+        Font(R.font.gothic_medium, FontWeight.Medium),
+        Font(R.font.gothic_semibold, FontWeight.SemiBold),
+        Font(R.font.gothic_bold, FontWeight.Bold),
+        Font(R.font.gothic_black, FontWeight.Black)
+    )
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        color = AquaBlue,
+        fontFamily = gothicA1,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 14.sp
+    ),
+    displayMedium = TextStyle(
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        color = TextWhite,
+        fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )

5f7ecae562953d199a954be4737346d3f31855cf
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-04-08 22:52:39 +0100

    fix: fixed the gradle bug

--- app/src/main/java/com/example/meditationui/MainActivity.kt
+++ app/src/main/java/com/example/meditationui/MainActivity.kt
@@ -10,37 +10,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationui.ui.HomeScreen
 import com.example.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MeditationUITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+//            MeditationUITheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+                    HomeScreen()
                 }
-            }
-        }
+           // }
+        //}
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MeditationUITheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
---
+++ app/src/main/java/com/example/meditationui/ui/HomeScreen.kt
@@ -0,0 +1,22 @@
+package com.example.meditationui.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.meditationui.ui.theme.DeepBlue
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+
+        }
+    }
+}
